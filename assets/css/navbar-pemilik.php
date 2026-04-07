6a7c42190dc594d20ab28d87de422d9ca1d78b37
Kaysan <80654004+selamat590@users.noreply.github.com>
2026-04-08 06:23:17 +0700

    Add files via upload

---
+++ assets/css/navbar-pemilik.php
@@ -0,0 +1,10 @@
+<header class="bg-black text-white pb-10">
+        <nav class=" mx-auto px-3 py-3 flex justify-between items-center">
+            <div class="text-3xl font-bold italic">Wonder<span class="text-red-600">Moto</span></div>
+            <ul class="hidden md:flex space-x-8 text-sm uppercase tracking-widest">
+                <li><a href="../pemilik/dashboard_pemilik.php" class="hover:text-red-500">Home</a></li>
+                <li><a href="../pemilik/tambah_motor.php" class="hover:text-red-500">Tambah motor</a></li>
+            </ul>
+            <a class="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 transition-all duration-200" href='../logout.php'>Logout</a>
+        </nav>
+</header>
---
+++ assets/css/navbar-penyewa.php
@@ -0,0 +1,11 @@
+<header class="bg-black text-white pb-10">
+        <nav class=" mx-auto px-3 py-3 flex justify-between items-center">
+            <div class="text-3xl font-bold italic">Wonder<span class="text-red-600">Moto</span></div>
+            <ul class="hidden md:flex space-x-8 text-sm uppercase tracking-widest">
+                <li><a href="../public/dashboard_penyewa.php" class="hover:text-red-500">Home</a></li>
+                <li><a href="../public/history.php" class="hover:text-red-500">History & pembayaran</a></li>
+
+            </ul>
+            <a class="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 transition-all duration-200" href='../logout.php'>Logout</a>
+        </nav>
+</header>
